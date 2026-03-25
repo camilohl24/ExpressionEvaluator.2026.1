39baaa6bc5cb930145a49609842dd3247cae2a87
camilohl24 <juancamilohernandezhl@gmail.com>
2026-03-25 18:29:22 -0500

    Create stack for postfix to separate numbers of more than one digit and modifed the method EvaluatePostfix for that it receives Stacks.

--- ExpressionEvaluator.Core/Evaluator.cs
+++ ExpressionEvaluator.Core/Evaluator.cs
@@ -1,17 +1,20 @@
-﻿namespace ExpressionEvaluator.Core;
+﻿using System.Collections;
+using System.Globalization;
+
+namespace ExpressionEvaluator.Core;
 
 public class Evaluator
 {
     public static double Evaluate(string infix)
     {
-        var postfix = InfixToPostfix(infix);
+        var postfix = InfixToPostfix(infix).ToArray().Reverse();
         return EvaluatePostfix(postfix);
     }
 
-    private static double EvaluatePostfix(string postfix)
+    private static double EvaluatePostfix(IEnumerable<string> postfix)
     {
         var stack = new Stack<double>();
-        foreach (char item in postfix)
+        foreach (string item in postfix)
         {
             if (IsOperator(item))
             {
@@ -19,30 +22,36 @@ public class Evaluator
                 var a = stack.Pop();
                 stack.Push(item switch
                 {
-                    '+' => a + b,
-                    '-' => a - b,
-                    '*' => a * b,
-                    '/' => a / b,
-                    '^' => Math.Pow(a, b),
+                    "+" => a + b,
+                    "-" => a - b,
+                    "*" => a * b,
+                    "/" => a / b,
+                    "^" => Math.Pow(a, b),
                     _ => throw new Exception("Sintax error."),
                 });
             }
             else
             {
-                stack.Push(double.Parse(item.ToString()));
+                stack.Push(double.Parse(item, CultureInfo.InvariantCulture));
             }
         }
         return stack.Pop();
     }
 
-    private static string InfixToPostfix(string infix)
+    private static Stack<string> InfixToPostfix(string infix)
     {
-        var postFix = string.Empty;
+        var temp = string.Empty;
+        var stackPostfix = new Stack<string>();
         var stack = new Stack<char>();
         foreach (var item in infix)
         {
             if (IsOperator(item))
             {
+                if (ValidateLenght(temp))
+                {
+                    stackPostfix.Push(temp);
+                    temp = string.Empty;
+                }
                 if (stack.Count == 0)
                 {
                     stack.Push(item);
@@ -54,7 +63,7 @@ public class Evaluator
                     {
                         do
                         {
-                            postFix += stack.Pop();
+                            stackPostfix.Push(stack.Pop().ToString());
                         } while (stack.Peek() != '(');
                         stack.Pop();
                     }
@@ -66,7 +75,7 @@ public class Evaluator
                         }
                         else
                         {
-                            postFix += stack.Pop();
+                            stackPostfix.Push(stack.Pop().ToString());
                             stack.Push(item);
                         }
                     }
@@ -74,15 +83,21 @@ public class Evaluator
             }
             else
             {
-                postFix += item;
+                temp += item;
             }
+        }
+        if (ValidateLenght(temp))
+        {
+            stackPostfix.Push(temp);
         }
         while (stack.Count > 0)
         {
-            postFix += stack.Pop();
+            stackPostfix.Push(stack.Pop().ToString());
         }
-        return postFix;
+        return stackPostfix;
     }
+
+    private static bool IsOperator(string item) => "+-*/^()".Contains(item);
 
     private static bool IsOperator(char item) => "+-*/^()".Contains(item);
 
@@ -107,4 +122,6 @@ public class Evaluator
         '(' => 0,
         _ => throw new Exception("Sintax error."),
     };
+
+    private static bool ValidateLenght(string temporal) => temporal.Length > 0;
 }
